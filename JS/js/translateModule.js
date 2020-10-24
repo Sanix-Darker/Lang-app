993d402c9f2fad5ec07adf618ccad7b7f21501b9
sanix-darker <s4nixd@gmail.com>
2020-10-24 08:21:59 +0100

    Updates

--- JS/js/translateModule.js
+++ JS/js/translateModule.js
@@ -15,16 +15,13 @@
  * @param {*} lang 
  */
 const translateModule = (lang="en") => {
-
 	langSet = lang;
 
 	// if the langCOde provide is in our array
 	if(langArray.includes(lang) || (localStorage.getItem("lang_app") === null && langArray.includes(lang))){
-		
 		// We save that in the browser as a defalt setting
 		localStorage.setItem("lang_app", langSet);
 	}else if (localStorage.getItem("lang_app") !== null){ 
-
 		// if Not For the local storage
 		langSet = localStorage.getItem("lang_app");
 	}else{
@@ -35,11 +32,9 @@ const translateModule = (lang="en") => {
 
 	// We are gonna loop all over those node elements
 	const nodesArray = [].slice.call(document.querySelectorAll("[translate]"));
-
 	[].forEach.call(nodesArray, (element) => {
 		element.innerHTML = langKeyValue[langArray.indexOf(langSet)][element.attributes.translate.nodeValue];
 	});
-
 }
 
 translateModule(langSet);
